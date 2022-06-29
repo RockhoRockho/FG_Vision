2e76e62eaf586f2960b2aa63126cc539c36d110f
playpizza <pzzaq@naver.com>
2022-06-29 10:34:58 +0900

    update basic
    기본 형태 작성

--- VISION/urls.py
+++ VISION/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.vision, name='vision'),
+    path('admin/', views.vision, name='admin_form_view'),
+    path('admin/make/', views.vision, name='admin_form_make'),
 ]
--- VISION/views.py
+++ VISION/views.py
@@ -4,3 +4,19 @@ def vision(request):
     context = {}
     
     return render(request, 'vision.html', context)
+
+
+def admin_form_view(request):
+    context = {
+
+    }
+    
+    return render(request, 'admin_form_view.html')
+
+
+def admin_form_make(request):
+    context = {
+
+    }
+    
+    return render(request, 'admin_form_make.html')
