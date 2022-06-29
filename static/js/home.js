ce1ca479eb285d79abe84756006fb57605f5c892
yoo-wonjun <85600188+yoo-wonjun@users.noreply.github.com>
2022-06-29 22:05:36 +0900

    메인페이지 골조

--- static/js/home.js
+++ static/js/home.js
@@ -1,11 +1,12 @@
 function preview(event) {
-  var reader = new FileReader();
+    var reader = new FileReader();
 
-  reader.onload = function (event) {
-    var img = document.createElement("img");
-    img.setAttribute("src", event.target.result);
-    document.querySelector("div.prepre").appendChild(img).style.width='300px';
-  };
+    reader.onload = function (event) {
+        var img = document.createElement("img");
+        img.setAttribute("src", event.target.result);
+        document.querySelector("div.prepre").appendChild(img).style.width = '300px';
+    };
 
-  reader.readAsDataURL(event.target.files[0]);
+    reader.readAsDataURL(event.target.files[0]);
 }
+
