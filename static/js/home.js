a83016fad231abe777e4624d09fe77c6369c7d53
yoo-wonjun <85600188+yoo-wonjun@users.noreply.github.com>
2022-06-29 20:49:30 +0900

    base.html 수정

---
+++ static/js/home.js
@@ -0,0 +1,11 @@
+function preview(event) {
+  var reader = new FileReader();
+
+  reader.onload = function (event) {
+    var img = document.createElement("img");
+    img.setAttribute("src", event.target.result);
+    document.querySelector("div.prepre").appendChild(img).style.width='300px';
+  };
+
+  reader.readAsDataURL(event.target.files[0]);
+}
