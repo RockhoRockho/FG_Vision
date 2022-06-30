f675f3205d8ba3c7378e8fe5bca9f18e950e15e5
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-06-30 14:40:52 +0900

    드래그앤 드롭

--- static/js/home.js
+++ static/js/home.js
@@ -1,12 +1,64 @@
 function preview(event) {
+
     var reader = new FileReader();
 
     reader.onload = function (event) {
         var img = document.createElement("img");
         img.setAttribute("src", event.target.result);
-        document.querySelector("div.prepre").appendChild(img).style.width = '300px';
+        document.querySelector("#preview").appendChild(img).style.width = '300px';
     };
 
     reader.readAsDataURL(event.target.files[0]);
 }
 
+const InputImage = document.getElementById('input_file')
+InputImage.addEventListener('change', event => {
+    preview(event)
+})
+
+$('#preview')
+    .on("dragover", dragOver)
+    .on("dragleave", dragOver)
+    .on("drop", uploadFiles);
+
+function dragOver(e){
+    e.stopPropagation();
+    e.preventDefault();
+    if (e.type == "dragover") {
+        $(e.target).css({
+        "background-color": "black",
+        "outline-offset": "-20px"
+        });
+    } else {
+        $(e.target).css({
+        "background-color": "white",
+        "outline-offset": "-10px"
+        });
+        }
+}
+
+function uploadFiles(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    dragOver(e);
+    
+    $(e.target).css({});
+    
+    e.dataTransfer = e.originalEvent.dataTransfer;
+    var files = e.target.files || e.dataTransfer.files;
+
+    if (files.length > 1) {
+        alert('하나만 올려라.');
+        return;
+    }
+    if (files[0].type.match(/image.*/)) {
+        $(e.target).css({
+            "background-image": "url(" + window.URL.createObjectURL(files[0]) + ")",
+            "outline": "none",
+            "background-size": "100% 100%"
+        });
+    } else {
+        alert('이미지가 아닙니다.');
+        return;
+    }   
+}
