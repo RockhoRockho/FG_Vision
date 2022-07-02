fb03720f677758b240fc55c47ba105cee62c7d54
playpizza <pzzaq@naver.com>
2022-07-03 02:48:03 +0900

    update make 페이지
    update make 페이지
    
    사각형 마우스로 잡아서 이동 추가
    사각형 선을 잡아서 크기 조절 추가
    
    canvas가 설정된 크기로 되도록 고정되도록 클래스 수정
    
    html에 있는 js함수 js파일로 이동(getDateObject)

--- static/js/form_make.js
+++ static/js/form_make.js
@@ -1,6 +1,10 @@
+// 상수설정
+SIZE_RANGE = 6;
+
 // 캔버스요소 가져오기  
 var canvas = document.getElementById("p-canvas");
 
+// 사각형을 그리는 함수
 function drawRect(d) {
     // 드로잉요 컨텍스트 생성
     var a = canvas.getContext("2d");
@@ -12,6 +16,7 @@ function drawRect(d) {
     a.strokeRect(d["x"], d["y"], d["w"], d["h"]);
 };
 
+// 모든 사각형을 지우고 새로 그려주는 함수
 function draw(d) {
     // 캔버스 지우기
     var a = canvas.getContext("2d");
@@ -25,7 +30,10 @@ function draw(d) {
 };
 draw(data);
 
+// 
 
+
+// 데이터 복사해주는 함수 (data[i]에 target을 복사해서 넣는데 사용됨)
 function dataCopy(d, t) {
     d[t["idx"]]["x"] = t["x"];
     d[t["idx"]]["y"] = t["y"];
@@ -59,24 +67,101 @@ function keyDownHandler(e) {
 elem = document.querySelector('canvas');
 canvasX = 0;
 canvasY = 0;
+isPress = false;
 
+// 마우스 움직일때의 이벤트 함수
 // 캔버스 내에서의 마우스 위치 잡기
 function mousemove(event){
     let rect = elem.getBoundingClientRect();
-    if (rect.x <= event.clientX & event.clientX <= (rect.x + rect.width) & rect.y <= event.clientY & event.clientY <= (rect.y + rect.height)){
-        // console.log("canvasX : " + (event.clientX - rect.x) + "\t canvasY : " + (event.clientY - rect.y));
-        tempX = event.clientX - rect.x;
-        tempY = event.clientY - rect.y;
+    
+    tempX = event.clientX - rect.x;
+    tempY = event.clientY - rect.y;
+    
+    moveX = tempX - canvasX;
+    moveY = tempY - canvasY;
+    
+    canvasX = tempX;
+    canvasY = tempY;
+    
+    // console.log('move:', moveX, moveY);
+    // console.log('offset', canvasX, canvasY);
+    
+    // 마우스 클릭상태인지 체크
+    if (isPress == true){
+        // 캔버스 내부인지 체크
+        // 캔버스 크기가 고정일때만 정상작동됨 -> 크기가 바뀌어도 작동하려면 비율계산식 추가필요
+        if (rect.x <= event.clientX & event.clientX <= (rect.x + rect.width) & rect.y <= event.clientY & event.clientY <= (rect.y + rect.height)){
+            // 타겟 사각형 안쪽인지 확인
+            if (target['x'] < canvasX & canvasX < (target['x'] + target['w']) & target['y'] < canvasY & canvasY < (target['y'] + target['h'])){
+                // 마우스 움직임만큼 크기 더하기
+                target['x'] += moveX;
+                target['y'] += moveY;
+    
+                data = dataCopy(data, target);
+                draw(data);
+            }
 
-        moveX = tempX - canvasX;
-        moveY = tempY - canvasY;
 
-        canvasX = tempX;
-        canvasY = tempY;
-        
-        console.log('move:', moveX, moveY);
-        console.log('offset', canvasX, canvasY);
+            // 타겟의 외곽선인지 확인
+            // 왼쪽선
+            if (target['x'] - SIZE_RANGE < canvasX & canvasX < target['x'] + SIZE_RANGE) {
+                target['x'] += moveX;
+                target['w'] -= moveX;
+
+                data = dataCopy(data, target);
+                draw(data);
+            }
+
+            // 오른쪽선
+            if (target['x'] + target['w'] - SIZE_RANGE < canvasX & canvasX < target['x'] + target['w'] + SIZE_RANGE) {
+                target['w'] += moveX;
+
+                data = dataCopy(data, target);
+                draw(data);
+            }
+
+            // 위쪽선
+            if (target['y'] - SIZE_RANGE < canvasY & canvasY < target['y'] + SIZE_RANGE) {
+                target['y'] += moveY;
+                target['h'] -= moveY;
+
+                data = dataCopy(data, target);
+                draw(data);
+            }
+
+            // 아래쪽선
+            if (target['y'] + target['h'] - SIZE_RANGE < canvasY & canvasY < target['y'] + target['h'] + SIZE_RANGE) {
+                target['h'] += moveY;
+
+                data = dataCopy(data, target);
+                draw(data);
+            }
+
+            // 솔직히 꼭지점은 귀찮은데 안하면 안되려나
+
+        }
     }
 }
 
+// 마우스 클릭시 이벤트 함수
+function mousedown(event){
+    isPress = true;
+    // 해당 위치 확인후 마우스 모양과 작동 바꾸기
+}
+
+// 마우스 클릭 해제시 이벤트 함수
+function mouseup(event){
+    isPress = false;
+}
+
+
+// 이벤트 추가
+window.addEventListener('mousedown', mousedown);
+window.addEventListener('mouseup', mouseup);
 window.addEventListener('mousemove', mousemove);
+
+// ?
+function getDateObject(key, value) {
+    if (key === 'date') return new Date(value);
+    return value;
+}
