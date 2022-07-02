26eceaa8db88fccd884c369d1b5a3f67f0f8861d
playpizza <pzzaq@naver.com>
2022-07-02 16:33:49 +0900

    home.html db데이터 변수에 넣기 코드
    home.html db데이터 변수에 넣기 코드 작성

--- MAIN/views.py
+++ MAIN/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from VISION.jsonbase import JsonBase
 
 def home(request):
     context = {}
+
+    j = JsonBase('jsonbase.json')
+
+
+    context['jsonData'] = j.all_data()
 
     return render(request, 'home.html', context)
--- VISION/jsonbase.py
+++ VISION/jsonbase.py
@@ -13,7 +13,7 @@ class JsonBase:
     # json_data를 파일로 저장
     # ret의 내림차순으로 정렬해서 저장
     def update_data(self, data):
-        with open(self.json_path, 'r') as f:
+        with open(self.json_path, 'r', encoding='utf-8') as f:
             json_data = json.load(f)
 
         json_data.append(data)
@@ -26,7 +26,7 @@ class JsonBase:
 
     # title에 해당하는 data 삭제 후 저장
     def delete_data(self, title):
-        with open(self.json_path, 'r') as f:
+        with open(self.json_path, 'r', encoding='utf-8') as f:
             json_data = json.load(f)
 
         idx = -1
@@ -45,7 +45,7 @@ class JsonBase:
     # data 검색
     # title과 같은것, ret보다 작은것중 가장 큰 것(없으면 가장 큰 것(최근것))
     def search_data(self, title="", ret=""):
-        with open(self.json_path, 'r') as f:
+        with open(self.json_path, 'r', encoding='utf-8') as f:
             json_data = json.load(f)
         
         if title != "":
@@ -73,7 +73,7 @@ class JsonBase:
     # 모든 데이터
     # title로 정렬해서 리턴
     def all_data(self):
-        with open(self.json_path, 'r') as f:
+        with open(self.json_path, 'r', encoding='utf-8') as f:
             json_data = json.load(f)
         json_data = sorted(json_data, key=(lambda x: x['form_title']))
         return json_data
